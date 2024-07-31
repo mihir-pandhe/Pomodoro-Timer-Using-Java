aae117c934592af377911ad02082f7fac4f93ec5
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-31 22:51:29 +0530

    Added Session Tracking

--- PomodoroTimer.java
+++ PomodoroTimer.java
@@ -8,6 +8,9 @@ public class PomodoroTimer {
     private static int breakTime;
     private static int timeRemaining;
     private static Timer timer;
+    private static boolean isWorkSession = true;
+    private static int completedWorkSessions = 0;
+    private static int completedBreakSessions = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -38,27 +41,19 @@ public class PomodoroTimer {
 
     private static void startWorkSession() {
         timeRemaining = workTime;
+        isWorkSession = true;
         System.out.printf("Work session started. Time remaining: %02d:%02d\n", workTime / 60, 0);
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                timeRemaining--;
-                if (timeRemaining <= 0) {
-                    timer.cancel();
-                    System.out.println("\nWork session ended. Take a break!");
-                    startBreakSession();
-                } else {
-                    int minutes = timeRemaining / 60;
-                    int seconds = timeRemaining % 60;
-                    System.out.printf("Time remaining: %02d:%02d\r", minutes, seconds);
-                }
-            }
-        }, 0, 1000);
+        startTimer();
     }
 
     private static void startBreakSession() {
         timeRemaining = breakTime;
+        isWorkSession = false;
         System.out.printf("Break session started. Time remaining: %02d:%02d\n", breakTime / 60, 0);
+        startTimer();
+    }
+
+    private static void startTimer() {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -66,8 +61,16 @@ public class PomodoroTimer {
                 timeRemaining--;
                 if (timeRemaining <= 0) {
                     timer.cancel();
-                    System.out.println("\nBreak session ended. Back to work!");
-                    startWorkSession();
+                    if (isWorkSession) {
+                        completedWorkSessions++;
+                        System.out.printf("\nWork session ended. Completed work sessions: %d\n", completedWorkSessions);
+                        startBreakSession();
+                    } else {
+                        completedBreakSessions++;
+                        System.out.printf("\nBreak session ended. Completed break sessions: %d\n",
+                                completedBreakSessions);
+                        startWorkSession();
+                    }
                 } else {
                     int minutes = timeRemaining / 60;
                     int seconds = timeRemaining % 60;
